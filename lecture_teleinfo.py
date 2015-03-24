5e417c7729874b5f3c6d3e9c7a30c2d22daa9c9e
Robin Roche <robin.roche@gmail.com>
2015-03-24 12:24:21 +0100

    Create lecture_teleinfo.py

---
+++ lecture_teleinfo.py
@@ -0,0 +1,36 @@
+import serial
+import time
+import string
+import urllib
+ 
+ser = serial.Serial('/dev/ttyAMA0', baudrate=1200, bytesize=serial.SEVENBITS, parity=serial.PARITY_EVEN, stopbits=serial.STOPBITS_ONE, timeout=0, rtscts=1)
+ 
+found = 0
+found_papp = 0
+found_ptec = 0
+ 
+while found==0:	
+	linecont = ser.readline()
+	# add checksum
+	if (string.find(linecont,'PAPP')!=-1 and found_papp==0):	
+		try:
+			papp = int(linecont[5:10])
+			if papp>=1:
+				found_papp = 1
+				print 'papp found:'
+				print papp
+		except ValueError:
+			print 'error'
+	if (string.find(linecont,'PTEC')!=-1 and found_ptec==0):
+		hphc = linecont[5:7]
+		if (hphc=='HC' or hphc=='HP'):
+			found_ptec = 1
+			print 'hphc found:'
+			print hphc
+	found = found_papp*found_ptec
+	time.sleep(1)
+ 
+ 
+url = "http://urlduserveur/upload_data.php?PASSWD=123456&LOAD=" + str(papp) + "&HPHC=" + str(hphc)
+print "Connecting to " + url
+response = urllib.urlopen(url).read()
